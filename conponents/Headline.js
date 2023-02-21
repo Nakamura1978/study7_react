b96632fc66ca3a5e745da1422ab3cef8aaa9eb76
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-22 08:51:02 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ conponents/Headline.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export default function Headline(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1 className={styles.title}>{props.page} Page</h1>
+
+            <div className={styles.description}>
+                <p>
+                    Get started by editing&nbsp;
+                    <code className={styles.code}>pages/{props.page}.js</code>
+                </p>
+                <div>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        By{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.vercelLogo}
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>    
+    )
+}
