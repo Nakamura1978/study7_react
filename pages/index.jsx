11c445fbcf0d04cc8048c4ee5b4c203233fb0982
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-23 19:50:58 +0900

    ✨ useEffectの導入

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
 import { Header } from 'src/conponents/Header'
 import Head from 'next/head'
 import { Main } from 'src/conponents/Main'
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    console.log("マウント");
+    document.body.style.backgroundColor = "liteblue";
+    return () => {
+      console.log("アンマウント")
+      document.body.style.backgroundColor = "";
+    }
+  }, []);
+  
   return (
     <>
       <Head>
