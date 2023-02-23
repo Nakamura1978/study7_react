dfc5866470c8cca1ec7f75872bab546fdc2f7f3e
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-23 17:51:33 +0900

    ⚗️ 不要なコードですが試しに書いてみました。いつか消します。

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,25 @@
 import { Header } from 'src/conponents/Header'
 import Head from 'next/head'
 import { Main } from 'src/conponents/Main'
+import { useCallback } from 'react';
 
 export default function Home() {
+  const foo = 1;
+  const bandleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+  
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={bandleClick} >
+        ボタン
+      </a>
       <Main page="index" />
     </>
   )
