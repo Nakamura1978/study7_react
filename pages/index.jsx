f0384160dde4d08e9de9499a829887962efb3c70
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-23 18:01:19 +0900

    ⚰️ 不要なコードの削除

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,22 +4,12 @@ import { Main } from 'src/conponents/Main'
 import { useCallback } from 'react';
 
 export default function Home() {
-  const foo = 1;
-  const bandleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
-  
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={bandleClick} >
-        ボタン
-      </a>
       <Main page="index" />
     </>
   )
